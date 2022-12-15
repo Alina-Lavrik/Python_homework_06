7231da247b8675252d96ccea0206eeaf9dadf361
Alina Lavrik <alina_voro@mail.ru>
2022-12-15 17:23:07 +0300

    Add Task_002

---
+++ TASK_02.py
@@ -0,0 +1,16 @@
+# 2. Напишите программу, которая на вход принимает 5 чисел 
+# и находит максимальное из них
+
+''' Вариант 1
+max = int(input())
+
+for i in range(4):
+    b = int(input())
+    if b > max:
+      max = b
+print(max) '''
+
+# Вариант 2
+
+a = list(map(int, '1 2 3 22 77'.split()))  
+print(max(a))
